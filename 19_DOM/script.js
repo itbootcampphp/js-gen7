986825220b8b27e6b7e32b18349ed7451380a705
stefanstanimirovic <stanimirovic.stefan@gmail.com>
2021-12-28 17:10:08 +0100

    DOM nastavak

--- 19_DOM/script.js
+++ 19_DOM/script.js
@@ -53,3 +53,49 @@ console.log(paragrafi);
 
 linkovi = document.querySelectorAll("[name='link']");
 console.log(linkovi);
+
+p1.innerHTML += "Promenjen <span>tekst</span> paragrafa";
+
+let sviLinkovi = document.querySelectorAll("a");
+
+sviLinkovi.forEach(link => {
+    // HTML atributi
+    link.href = "https://www.google.com";
+    link.target = "_blank";
+
+    // CSS stilovi
+    link.style.color = "green";
+    link.style.textDecoration = "none";
+
+    //link.style = "color: green; text-decoration: none;";
+
+    // Preko metode
+    link.setAttribute('name', 'link2'); // isto kao i link.name = "link2";
+    link.setAttribute("style", "color: green; text-decoration: none;"); // isto kao i link.style = "...";
+});
+
+
+// Svako u novom linku. Ako ime počinje na slovo „S“, link se otvara u novom tabu, a inače se otvara na istoj stranici.
+
+let imena = ["Stefan", "Marija", "Nikola", "Bogdan", "Vladan"];
+imena.forEach(ime => {
+    if(ime.startsWith("S")) { //if(ime[0] == "S") {
+        document.body.innerHTML += `<a href="#" target="_blank">${ime}</a>`;
+    }
+    else {
+        document.body.innerHTML += `<a href="#">${ime}</a>`;
+    }
+});
+
+// U listi kao stavke liste. Naizmenično stavke liste obojiti sa dve različite boje.
+let listaString = "<ul>";
+imena.forEach((ime, i) => {
+    if(i % 2 == 0) {
+        listaString += `<li style="color: red">${ime}</li>`;
+    }
+    else {
+        listaString += `<li style="color: blue">${ime}</li>`;
+    }
+});
+listaString += "</ul>";
+document.body.innerHTML += listaString;
