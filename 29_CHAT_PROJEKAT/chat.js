e39a632176807f438f5bef27de58cdd73f2eebc1
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2022-01-31 17:10:30 +0100

    TASK_5 Dodavanje callback funkcije za ispis podataka

--- 29_CHAT_PROJEKAT/chat.js
+++ 29_CHAT_PROJEKAT/chat.js
@@ -40,6 +40,19 @@ class Chatroom{
         let response = await this.chats.add(docChat);
         return response; // Vraćam Promise i mogu za njega da kažem .then i .catch            
     }   
+
+    // Metod koji prati promene u bazi i vraća poruke
+    getChats(callback) {
+        this.chats.onSnapshot( snapshot => {
+            snapshot.docChanges().forEach( change => {
+                // Ispiati dokumenete koji su dodati u bazu
+                if(change.type == "added") {
+                    // console.log(change.doc.data());
+                    callback(change.doc.data()); // prosleđivanje dokumenta na ispis (ispis realizujemo kada realizujemo callback funkciju)
+                }
+            });
+        });
+    }
 }
 
 let chatroom1 = new Chatroom("js", "Stefan");
@@ -50,8 +63,12 @@ chatroom1.room = "general"; // Testiram seter za room
 console.log(chatroom1.room);
 
 let chatroom2 = new Chatroom("general", "Milena");
-chatroom2.addChat("Trening pisanja CV-ja")
-    .then( () => console.log("Uspešno dodat čet"))
-    .catch( err => console.log(err));
+// chatroom2.addChat("Trening pisanja CV-ja")
+//     .then( () => console.log("Uspešno dodat čet"))
+//     .catch( err => console.log(err));
+
+chatroom2.getChats(d => {
+    console.log(d);
+});
 
 
