40810b9436ed42c0bbf27a5a9913bb999caff4ee
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2022-02-01 16:24:08 +0100

    TASK_9 Formatiranje vremena u poruci

--- 29_CHAT_PROJEKAT/ui.js
+++ 29_CHAT_PROJEKAT/ui.js
@@ -13,18 +13,38 @@ export class ChatUI{
         return this._list;
     }
 
+    formatDate(date){        
+        let d = date.getDate();
+        let m = date.getMonth() + 1;
+        let y = date.getFullYear();
+        let h = date.getHours();
+        let min = date.getMinutes();
+
+        //Dodavanje 0 ispred jednocifrenih vrednosti
+        d = String(d).padStart(2,"0");
+        m = String(m).padStart(2,"0");
+        h = String(h).padStart(2,"0");
+        min = String(min).padStart(2, "0");
+
+        let strDate = d + "." + m + "." + y + ". - " + h + ":" + min;
+
+        return strDate;
+    }
+
     templateLI(data) {
         // Izvući podatke iz prosleđenog dokuemnat i zapisati ih u formatu
         // useranme: poruka
         // time_stamp
 
+        let date = data.created_at.toDate();
+        let strDate = this.formatDate(date);
+
         let htmlLI = 
         `<li>
-            ${data.username} : ${data.message}
-            <br>
-            ${data.created_at}
+            <span class="username">${data.username} : </span>
+            <span class="message">${data.message}</span>
+            <div class="date">${strDate}</div>
         </li>`;
-
         this.list.innerHTML += htmlLI;
     }
 } 
