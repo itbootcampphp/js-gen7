9ea2a5f20348adaa36ee3409ae723879e997534b
JelenaMatejic <jelena.jelena.matejic@gmail.com>
2022-02-03 14:03:55 +0100

    TASK_12 Organizacija koda i reimenovanje varijabli

--- 29_CHAT_PROJEKAT/app.js
+++ 29_CHAT_PROJEKAT/app.js
@@ -1,42 +1,32 @@
 import {Chatroom} from './chat.js';
 import {ChatUI} from './ui.js';
 
-let chatroom1 = new Chatroom("js", "Stefan");
-console.log(chatroom1.username, chatroom1.room); // Testiram getere
-chatroom1.username = "Dušan"; // Testiram seter za username
-console.log(chatroom1.username);
-chatroom1.room = "general"; // Testiram seter za room
-console.log(chatroom1.room);
-
-let chatroom2 = new Chatroom("general", "Milena");
-// chatroom2.addChat("Trening pisanja CV-ja")
-//     .then( () => console.log("Uspešno dodat čet"))
-//     .catch( err => console.log(err));
-
-// Ispis dokumeneata iz db u konzoli
-chatroom2.getChats(d => {
-    console.log(d);
-});
-
-let ulChatList = document.querySelector('ul');
-let chatUI1 = new ChatUI(ulChatList);
-
-// Ispis dokumenata iz db na stranici
-chatroom2.getChats(d => {
-    chatUI1.templateLI(d);
-});
-
 // DOM
+let ulChatList = document.querySelector('ul');
 let formMessage = document.querySelector('#formMessage');
 let inputMessage = document.querySelector('#inputMessage');
 let formUsername = document.querySelector('#formUsername');
 let inputUsername = document.querySelector('#inputUsername');
 
+// Objekti klasa / Instance klasa
+let chatroom = new Chatroom("js", "Stefan");
+let chatUI = new ChatUI(ulChatList);
+
+// Ispis dokumeneata iz db u konzoli
+chatroom.getChats(d => {
+    console.log(d);
+});
+
+// Ispis dokumenata iz db na stranici
+chatroom.getChats(d => {
+    chatUI.templateLI(d);
+});
+
 //Kada je submit dugme Send pošalji poruku
 formMessage.addEventListener('submit' , e => {
     e.preventDefault();
     let message = inputMessage.value;
-    chatroom2.addChat(message)
+    chatroom.addChat(message)
         .then( () => formMessage.reset())
         .catch( error => console.log(error));
 });
@@ -45,6 +35,6 @@ formMessage.addEventListener('submit' , e => {
 formUsername.addEventListener('submit', e => {
     e.preventDefault();
     let newUsername = inputUsername.value;
-    chatroom2.username = newUsername;
+    chatroom.username = newUsername;
     formUsername.reset();
 });
